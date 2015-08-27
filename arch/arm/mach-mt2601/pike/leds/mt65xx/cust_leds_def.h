58745ec7b4a5a1efc7b3a377ee7e73972c824f76
Jyunwei Lin <jyunwei.lin@mediatek.com>
2015-08-27 17:17:02 +0800

    arm: polar: change backlight design
    change backlight from isink0 to isink0 and isink1
    
    Change-Id: Ie3cf459e5e9cda292488a2a560e78226600451fd
    Signed-off-by: Jyunwei Lin <jyunwei.lin@mediatek.com>

--- arch/arm/mach-mt2601/pike/leds/mt65xx/cust_leds_def.h
+++ arch/arm/mach-mt2601/pike/leds/mt65xx/cust_leds_def.h
@@ -1,3 +1,17 @@
+/*
+* Copyright (C) 2011-2015 MediaTek Inc.
+*
+* This program is free software: you can redistribute it and/or modify it under the terms of the
+* GNU General Public License version 2 as published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with this program.
+* If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _CUST_LEDS_DEF_H
 #define _CUST_LEDS_DEF_H
 
@@ -16,6 +30,7 @@
 enum mt65xx_led_pmic {
 	MT65XX_LED_PMIC_LCD_ISINK = 0,
 	MT65XX_LED_PMIC_LCD_ISINK0,
+	MT65XX_LED_PMIC_LCD_ISINK01,
 	MT65XX_LED_PMIC_NLED_ISINK0,
 	MT65XX_LED_PMIC_NLED_ISINK1,
 	MT65XX_LED_PMIC_NLED_ISINK2,
